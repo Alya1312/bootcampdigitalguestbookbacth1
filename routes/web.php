4dabd7619ef3791fd993c69f9de8484e5f707e16
pasukanbocil <ctian8787@gmail.com>
2023-11-24 18:19:17 +0700

    Submission 2

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,23 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Rute untuk menampilkan daftar pengguna
+Route::get('/users', [UserController::class, 'index']);
+
+// Rute untuk menampilkan formulir pembuatan pengguna
+Route::get('/users/create', [UserController::class, 'create']);
+
+// Rute untuk menyimpan pengguna baru
+Route::post('/users', [UserController::class, 'store']);
+
+// Rute untuk menampilkan detail pengguna
+Route::get('/users/{id}', [UserController::class, 'show']);
+
+// Rute untuk menampilkan formulir pengeditan pengguna
+Route::get('/users/{id}/edit', [UserController::class, 'edit']);
+
+// Rute untuk memperbarui informasi pengguna
+Route::put('/users/{id}', [UserController::class, 'update']);
+
+// Rute untuk menghapus pengguna
+Route::delete('/users/{id}', [UserController::class,'destroy']);
